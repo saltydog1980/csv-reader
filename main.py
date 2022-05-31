dfee6825702626769d782167467a591b8dbb615b
saltydog1980 <mikeelambert99@gmail.com>
2022-05-31 16:47:28 -0400

    final code

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import csv
 def animal_finder(animal):
     try:
-        with open(f'/Users/lamberto/codeExercises/Day6/csv-reader/data/{animal}s.csv', encoding="utf-8") as f: 
+        with open(f'/Users/lamberto/codeExercises/Day6/csv-reader/data/{animal}s.csv') as f: 
             reader = csv.DictReader(f)
             for row in reader:
                 print(f"{row['name']} is a{row[' age']} year old{row[' breed']}.")
     except:
         print(f"Sorry, we don't have any {animal}s here")
 
-animal_finder('fish')
+animal_finder('dog')
